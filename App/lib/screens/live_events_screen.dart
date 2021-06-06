aff853e2a7e31e09c8a369ad61fd465bd15f9787
niloysikdar <niloysikdar30@gmail.com>
2021-06-06 23:19:34 +0530

    Bug Fixes

--- App/lib/screens/live_events_screen.dart
+++ App/lib/screens/live_events_screen.dart
@@ -15,59 +15,63 @@ class _LiveEventsState extends State<LiveEvents> {
       child: Scaffold(
         body: Column(
           children: [
-            Container(
-              width: size.width,
-              padding: EdgeInsets.only(
-                top: 15,
-                right: 25,
-                bottom: 20,
-                left: 25,
-              ),
-              decoration: BoxDecoration(
-                color: kblue,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20),
-                ),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Live\nEvents",
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w600,
-                      letterSpacing: 8,
-                    ),
-                  ),
-                  SizedBox(height: 18),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: liveEventsCategories.map((item) {
-                      return Column(
-                        children: [
-                          Image.asset(item.image),
-                          SizedBox(
-                            height: (item.category != "Jet Ski") ? 9 : 0,
-                          ),
-                          Text(
-                            item.category,
-                            style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ],
-                      );
-                    }).toList(),
-                  ),
-                ],
-              ),
-            ),
+            header(size: size),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget header({Size size}) {
+    return Container(
+      width: size.width,
+      padding: EdgeInsets.only(
+        top: 15,
+        right: 25,
+        bottom: 20,
+        left: 25,
+      ),
+      decoration: BoxDecoration(
+        color: kblue,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(20),
+          bottomRight: Radius.circular(20),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Live\nEvents",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w600,
+              letterSpacing: 8,
+            ),
+          ),
+          SizedBox(height: 18),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: liveEventsCategories.map((item) {
+              return Column(
+                children: [
+                  Image.asset(item["image"]),
+                  SizedBox(
+                    height: (item["category"] != "Jet Ski") ? 9 : 0,
+                  ),
+                  Text(
+                    item["category"],
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
