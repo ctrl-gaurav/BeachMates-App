9ddb6d2389bc9d71775afa4fd290e8d40a21b31c
ctrl-gaurav <gauravhhh30@gmail.com>
2021-06-11 09:25:55 +0530

    Login Sign Up Screens Added

---
+++ App/lib/screens/login_signup/components/rounded_input_field.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:b2b/screens/login_signup/components/text_field_container.dart';
+import 'package:b2b/screens/login_signup/constants.dart';
+
+class RoundedInputField extends StatelessWidget {
+  final String hintText;
+  final IconData icon;
+  final ValueChanged<String> onChanged;
+  const RoundedInputField({
+    Key key,
+    this.hintText,
+    this.icon = Icons.person,
+    this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFieldContainer(
+      child: TextField(
+        onChanged: onChanged,
+        cursorColor: kPrimaryColor,
+        decoration: InputDecoration(
+          icon: Icon(
+            icon,
+            color: kPrimaryColor,
+          ),
+          hintText: hintText,
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+}
