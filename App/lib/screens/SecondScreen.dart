8a19d461dc795e18448083be95cd406d4187c209
niloysikdar <niloysikdar30@gmail.com>
2021-06-12 15:32:05 +0530

    Changes

--- App/lib/screens/SecondScreen.dart
+++ App/lib/screens/SecondScreen.dart
@@ -1,3 +1,4 @@
+import 'package:b2b/constants/colors.dart';
 import 'package:b2b/screens/second_screen/live_events_header.dart';
 import 'package:b2b/widgets/sorting_text.dart';
 import 'package:flutter/material.dart';
@@ -13,235 +14,87 @@ class _SecondScreenState extends State<SecondScreen> {
     Size size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              liveEventsHeader(size: size),
-              sortText(text: "Top Rated"),
-              Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: Container(
-                  height: 150,
-                  width: double.infinity,
-                  padding: EdgeInsets.all(20),
-                  margin: EdgeInsets.only(bottom: 20),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      image: DecorationImage(
-                          image: AssetImage('assets/images/item_1.jpg'),
-                          fit: BoxFit.cover),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey[400],
-                            blurRadius: 10,
-                            offset: Offset(0, 10))
-                      ]),
+        body: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Container(
+                margin: EdgeInsets.only(
+                  top: size.height * 0.35,
+                ),
+                child: Column(
+                  children: [
+                    secondCard(imagepath: "assets/images/item_1.jpg"),
+                    secondCard(imagepath: "assets/images/item_2.jpg"),
+                    secondCard(imagepath: "assets/images/item_3.jpg"),
+                    secondCard(imagepath: "assets/images/item_4.jpg"),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Container(
+                color: kwhite,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    liveEventsHeader(size: size),
+                    sortText(text: "Top Rated"),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget secondCard({
+    @required String imagepath,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 20,
+      ),
+      child: Container(
+        height: 150,
+        width: double.infinity,
+        padding: EdgeInsets.all(20),
+        margin: EdgeInsets.only(bottom: 20),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+              image: AssetImage(
+                imagepath,
+              ),
+              fit: BoxFit.cover,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey[400],
+                blurRadius: 10,
+                offset: Offset(0, 10),
+              )
+            ]),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                    children: <Widget>[],
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: Container(
-                  height: 150,
-                  width: double.infinity,
-                  padding: EdgeInsets.all(20),
-                  margin: EdgeInsets.only(bottom: 20),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      image: DecorationImage(
-                          image: AssetImage('assets/images/item_2.jpg'),
-                          fit: BoxFit.cover),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey[400],
-                            blurRadius: 10,
-                            offset: Offset(0, 10))
-                      ],
-                  ),
-                  child: Center(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        // Row(
-                        //   crossAxisAlignment: CrossAxisAlignment.center,
-                        //   children: <Widget>[
-                        //     Expanded(
-                        //       child: Column(
-                        //         crossAxisAlignment: CrossAxisAlignment.center,
-                        //         children: <Widget>[
-                        //
-                        //           SizedBox(
-                        //             width: 10,
-                        //           ),
-                        //           Text(
-                        //             "CITRONELLE",
-                        //             style: TextStyle(
-                        //               color: Colors.white,
-                        //               fontSize: 18,
-                        //             ),
-                        //           )
-                        //         ],
-                        //       ),
-                        //     ),
-                        //   ],
-                        // ),
-                        // Padding(
-                        //   padding: const EdgeInsets.all(8.0),
-                        //   child: Row(
-                        //     crossAxisAlignment: CrossAxisAlignment.center,
-                        //     children: <Widget>[
-                        //       SizedBox(width: 25,),
-                        //       Container(
-                        //         width: 5,
-                        //         height: 5,
-                        //         decoration: BoxDecoration(
-                        //             color: Colors.white,
-                        //             shape: BoxShape.circle),
-                        //       ),
-                        //       Expanded(
-                        //         child: Column(
-                        //           crossAxisAlignment: CrossAxisAlignment.center,
-                        //           children: <Widget>[
-                        //
-                        //             SizedBox(
-                        //               width: 10,
-                        //             ),
-                        //             Text(
-                        //               "Surf Boards",
-                        //               style: TextStyle(
-                        //                 color: Colors.white,
-                        //                 fontSize: 18,
-                        //               ),
-                        //             ),
-                        //
-                        //           ],
-                        //         ),
-                        //       ),
-                        //       Expanded(
-                        //         child: Row(
-                        //           children: [
-                        //             Container(
-                        //               width: 5,
-                        //               height: 5,
-                        //               decoration: BoxDecoration(
-                        //                   color: Colors.white,
-                        //                   shape: BoxShape.circle),
-                        //             ),
-                        //             SizedBox(width: 5,),
-                        //             Column(
-                        //               crossAxisAlignment: CrossAxisAlignment.center,
-                        //               children: <Widget>[
-                        //
-                        //                 SizedBox(
-                        //                   width: 10,
-                        //                 ),
-                        //                 Text(
-                        //                   "65-20 USD",
-                        //                   style: TextStyle(
-                        //                     color: Colors.white,
-                        //                     fontSize: 18,
-                        //                   ),
-                        //                 )
-                        //               ],
-                        //             ),
-                        //           ],
-                        //         ),
-                        //       ),
-                        //       Expanded(
-                        //         child: Row(
-                        //           children: [
-                        //             Container(
-                        //               width: 5,
-                        //               height: 5,
-                        //               decoration: BoxDecoration(
-                        //                   color: Colors.white,
-                        //                   shape: BoxShape.circle),
-                        //             ),
-                        //             SizedBox(width: 5,),
-                        //             Column(
-                        //               crossAxisAlignment: CrossAxisAlignment.center,
-                        //               children: <Widget>[
-                        //
-                        //                 SizedBox(
-                        //                   width: 10,
-                        //                 ),
-                        //                 Text(
-                        //                   "4.5 *",
-                        //                   style: TextStyle(
-                        //                     color: Colors.white,
-                        //                     fontSize: 18,
-                        //                   ),
-                        //                 )
-                        //               ],
-                        //             ),
-                        //           ],
-                        //         ),
-                        //       ),
-                        //     ],
-                        //   ),
-                        // ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: Container(
-                  height: 150,
-                  width: double.infinity,
-                  padding: EdgeInsets.all(20),
-                  margin: EdgeInsets.only(bottom: 20),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      image: DecorationImage(
-                          image: AssetImage('assets/images/item_3.jpg'),
-                          fit: BoxFit.cover),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey[400],
-                            blurRadius: 10,
-                            offset: Offset(0, 10))
-                      ]),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
       ),
     );
